310307a03ed8b9a48af9859182c4fd97d78094ce
Alex Mednick <32356860+err0rzzz@users.noreply.github.com>
2018-11-30 03:01:04 +0000

    all existing iterations minus files over 100mb

---
+++ CookingV2_Viewforia/Assets/scripts/buttonHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+
+public class buttonHandler : MonoBehaviour, IVirtualButtonEventHandler {
+
+    public GameObject button;
+
+	// Use this for initialization
+	void Start () {
+
+        button.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+   		
+	}
+	
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("ok!");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("nope");
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
